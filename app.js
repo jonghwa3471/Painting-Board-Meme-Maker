00f9d9b8f1579f6fd221056b352074ec47fd69bf
송종화 <songjonghwa@songjonghwaui-MacBookPro.local>
2024-02-14 19:30:15 +0900

    #2.3

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -28,9 +29,15 @@ const onLineWidthChange = (event) => {
   ctx.lineWidth = event.target.value;
 };
 
+const onColorChange = (event) => {
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+};
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
